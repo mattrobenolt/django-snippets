76430a1825afe739675b7ae613aadb9f95b06ff2
Matt Robenolt <matt@ydekproductions.com>
2011-08-26 14:34:34 -0400

    throttle.py is configurable via settings.

--- middleware/throttle.py
+++ middleware/throttle.py
@@ -1,5 +1,6 @@
 import os
 import time
+from django.conf import settings
 from django.http import HttpResponse
 from django.template import Context, RequestContext, loader
 
@@ -27,28 +28,81 @@ except ImportError:
                      return ncpus
          return 1 # Default
 
+if not getattr(settings, 'THROTTLE_CONFIG', None) or not isinstance(settings.THROTTLE_CONFIG, ThrottleConfig):
+    config = ThrottleConfig()
+    config.add(load=0.8, wait=0.25)
+    config.add(load=0.9, wait=0.5)
+    config.add(load=1.0, wait=1)
+    config.add(load=10)
+    THROTTLE_CONFIG = config
+else:
+    THROTTLE_CONFIG = settings.THROTTLE_CONFIG
+
+
+class ThrottleError(Exception):
+    pass
+
+class ThrottleConfig(object):
+    vals = {}
+    def add(self, load, wait=None):
+        if not isinstance(load, (int, float)):
+            raise ValueError("Load must be an int or float.")
+        if not load > 0:
+            raise ValueError("Load must be greater than 0")
+        if wait is not None and not isinstance(wait, (int, float)):
+            raise ValueError("Wait must be an int, float, or None")
+        elif wait is not None and not wait > 0:
+            raise ValueError("Wait must be greater than 0")
+        self.vals[load] = wait
+    
+    def dump(self):
+        return self.vals
+    
+    def __repr__(self):
+        return u"<ThrottleConfig %s>" % self.vals.__repr__()
+
 class KeepShitChill(object):
-    """
-    Basic request throttling so the server won't spiral out of control.
+    """Basic request throttling so the server won't spiral out of control.
 
     You must create a `503.html` page to display your own `failwhale`. :)
     """
+    def __init__(self):
+        config = THROTTLE_CONFIG.dump()
+        limits = config.keys()
+        limits.sort()
+        self.min = limits[0]
+
+        limits.reverse()
+        self.thresholds = [(k, THROTTLE_THRESHOLDS[k]) for k in limits]
+    
+    def __throttle(self, load):
+        to_throttle = 0
+        if load > self.min:
+            for limit in self.thresholds:
+                if load > limit[0]:
+                    to_throttle = limit[1]
+                    break
+            
+            if to_throttle is None:
+                raise ThrottleError("Server on fire!")
+        
+        return to_throttle
+
     def process_request(self, request):
         # Calculate total load based on number of CPUs.
         # e.g., a loadavg of 4 on a computer with 8 cores, the load is 0.5.
         load = os.getloadavg()[0] / float(cpu_count())
+
+        try:
+            to_throttle = self.__throttle(load)
+        except ThrottleError:
+            return server_on_fire(request)
         
-        if load > 0.8: # if loads are over 80%, start throttling
-            to_throttle = 0.25
-            
-            if load >= 0.9 and load < 1:
-                to_throttle = 0.5
-            elif load >= 1 and load < 10:
-                to_throttle = 1
-            elif load >= 10:
-                # Shit's on fire!
-                return server_on_fire(request)
+        if to_throttle > 0:
             time.sleep(to_throttle)
+    
+    def test(self, load):
+        return self.__throttle(load)
 
 def server_on_fire(request, template_name='503.html'):
     """
