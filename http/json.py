6b1c4a1d9f46ba59ece61001f2b244318dff23b7
Matt Robenolt <matt@ydekproductions.com>
2011-10-18 20:00:06 -0300

    Clean up, clean up, everybody, everywhere!

--- http/json.py
+++ http/json.py
@@ -13,8 +13,17 @@ except ImportError:
         from django.utils import simplejson as json
 
 # Reserved words list from http://javascript.about.com/library/blreserved.htm
-JAVASCRIPT_RESERVED_WORDS = tuple('abstract as boolean break byte case catch char class continue const debugger default delete do double else enum export extends false final finally float for function goto if implements import in instanceof int interface is long namespace native new null package private protected public return short static super switch synchronized this throw throws transient true try typeof use var void volatile while with'.split(' '))
-JAVASCRIPT_FUNCTION_PATTERN = re.compile(r'^[$a-z_][0-9a-z_]*$', re.I)
+JAVASCRIPT_RESERVED_WORDS = frozenset([
+    'abstract', 'as', 'boolean', 'break', 'byte', 'case', 'catch', 'char',
+    'class', 'continue', 'const', 'debugger', 'default', 'delete', 'do', 'double',
+    'else', 'enum', 'export', 'extends', 'false', 'final', 'finally', 'float',
+    'for', 'function', 'goto', 'if', 'implements', 'import', 'in', 'instanceof',
+    'int', 'interface', 'is', 'long', 'namespace', 'native', 'new', 'null',
+    'package', 'private', 'protected', 'public', 'return', 'short', 'static',
+    'super', 'switch', 'synchronized', 'this', 'throw', 'throws', 'transient',
+    'true', 'try', 'typeof', 'use', 'var', 'void', 'volatile', 'while', 'with',
+])
+JAVASCRIPT_FUNCTION_PATTERN = re.compile(r'^[$a-z_][0-9a-z_\.\[\]]*$', re.I)
 
 class JsonResponse(HttpResponse):
     def __init__(self, content, status=200, callback=None):
